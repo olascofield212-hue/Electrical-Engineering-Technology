254feacd8659979809fd244f88c3ef3619376687
olascofield212-hue <olascofield212@gmail.com>
2026-07-24 05:21:56 -0400

    Add files via upload

---
+++ sidebars.js
@@ -0,0 +1,13 @@
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  siteSidebar: [
+    {
+      type: 'category',
+      label: 'Electrical Engineering Technology Hub',
+      collapsed: false,
+      items: ['about', 'contact'],
+    },
+  ],
+};
+
+export default sidebars;
